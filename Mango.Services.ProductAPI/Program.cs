e578e19a6216396b98cb61cd550ac55b587635f7
Nursultan Yesbolatov <nyem@tengizchevroil.com>
2022-04-29 19:18:00 +0500

    mango project init

---
+++ Mango.Services.ProductAPI/Program.cs
@@ -0,0 +1,45 @@
+using AutoMapper;
+using Mango.Services.ProductAPI;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//DBContext Service
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+
+//IMapper Service
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//Dependency Injection
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+
+        Task<T> DeleteProductAsync<T>(int id);
+
+    }
+}
